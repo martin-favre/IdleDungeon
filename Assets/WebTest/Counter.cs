b03642083d52c0bb7078059b66cd4252899991b4
Martin Favre <martin.favre22@gmail.com>
2021-03-15 19:39:40 +0100

    Added testscene for webrelated things Added textmeshpro Added tile prefab

---
+++ Assets/WebTest/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Timers;
+
+public class Counter : MonoBehaviour
+{
+    int counter = 0;
+    public TMP_Text text;
+    Timer timer;
+    float offsetTime = 0;
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+        offsetTime = PlayerPrefs.HasKey("time") ? PlayerPrefs.GetFloat("time") : 0;
+    }
+
+    void Update()
+    {
+        float time = offsetTime + Time.unscaledTime;
+        text.text = time.ToString();
+        PlayerPrefs.SetFloat("time", time);
+    }
+}
