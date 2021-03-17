c0a3996e586d7a4682f2ff97e4ba5c35a3e461f1
Martin Favre <martin.favre22@gmail.com>
2021-03-17 20:51:58 +0100

    Made time more robust to increment and decrement in editor

--- Assets/Scripts/TimeManager/UnityTime.cs
+++ Assets/Scripts/TimeManager/UnityTime.cs
@@ -1,17 +1,34 @@
 using UnityEngine;
 public class UnityTime : ITimeProvider
 {
-    public float Time => UnityEngine.Time.unscaledTime*TimeScaling;
-    public float DeltaTime => UnityEngine.Time.unscaledDeltaTime*TimeScaling;
+    public float Time => timeOffset + UnityEngine.Time.unscaledTime * TimeScaling;
+    public float DeltaTime => timeOffset + UnityEngine.Time.unscaledDeltaTime * TimeScaling;
 
     private float timeScaling;
-    public float TimeScaling  { get => timeScaling; set => timeScaling = value; }
+    public float TimeScaling
+    {
+        get => timeScaling;
+        set
+        {
+            if(value < 0) {
+                Debug.LogWarning("Can't set timescaling to negative value");
+                return;
+            }
+            var oldTime = Time;
+            timeScaling = value;
+            if(Time < oldTime) {
+                timeOffset = oldTime;
+            }
+        }
+    }
 
     private static ITimeProvider instance;
 
+    private float timeOffset = 0;
     public static ITimeProvider Instance { get => instance; set => instance = value; }
 
-    static UnityTime() {
+    static UnityTime()
+    {
         instance = new UnityTime();
     }
 }
