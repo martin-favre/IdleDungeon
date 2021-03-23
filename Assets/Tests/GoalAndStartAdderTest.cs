a4ef44c2fc91c6111247f52fa2f63f5d8b8c47ec
Martin Favre <martin.favre22@gmail.com>
2021-03-23 23:03:21 +0100

    Repaired GoalAndStartAdderTest

--- Assets/Tests/GoalAndStartAdderTest.cs
+++ Assets/Tests/GoalAndStartAdderTest.cs
@@ -11,7 +11,8 @@ namespace Tests
     {
         Mock<IMap> mapMock;
         Vector2Int mapSize;
-        Tile mockTile;
+        Tile openTile;
+        Tile closedTile;
 
 
         [SetUp]
@@ -20,68 +21,61 @@ namespace Tests
             mapMock = new Mock<IMap>();
             mapSize = new Vector2Int(20, 20);
             mapMock.Setup(foo => foo.Size).Returns(mapSize);
-            mockTile = new Tile();
-            mockTile.SetAllWalls(true);
+            mapMock.Setup(foo => foo.InsideMap(It.IsAny<Vector2Int>())).Returns(true);
+            openTile = new Tile();
+            openTile.SetAllWalls(true);
+            closedTile = new Tile();
+            closedTile.SetAllWalls(false);
 
 
         }
 
-        // Tests disabled for now
-        // I don't know how to mock GridMap
-        // So it can reliably return a specific pattern of tiles
+        void SetUpSuccessfulSequence()
+        {
 
-        // [Test]
-        // public void GoalAndEndShouldBeSet()
-        // {
-        //     mapMock.Setup(foo => foo.GetTile(It.IsAny<Vector2Int>())).Returns(mockTile);
-        //     var adder = new GoalAndStartAdder();
-        //     adder.ImproveMap(mapMock.Object, 10);
-        //     mapMock.VerifySet(foo => foo.Goal = It.IsAny<Vector2Int>());
-        //     mapMock.VerifySet(foo => foo.Start = It.IsAny<Vector2Int>());
-        // }
+            // I don't know if this is so robust
+            // Bu it should return a sequence which will make the algorithm set a goal and a start
+            mapMock.SetupSequence(foo => foo.GetTile(It.IsAny<Vector2Int>())).
+                Returns(openTile).Returns(closedTile).Returns(closedTile).Returns(closedTile).
+                Returns(openTile).Returns(closedTile).Returns(closedTile).Returns(closedTile);
 
-        // [Test]
-        // public void GoalAndEndShouldBeSetEvenIfNotFirstTileIsFree()
-        // {
-        //     var blockedTile = new Tile();
-        //     blockedTile.SetAllWalls(false);
-        //     var openTile = new Tile();
-        //     openTile.SetAllWalls(true);
-        //     mapMock.SetupSequence(foo => foo.GetTile(It.IsAny<Vector2Int>()))
-        //         .Returns(blockedTile).Returns(blockedTile).Returns(openTile) // for goal
-        //         .Returns(blockedTile).Returns(blockedTile).Returns(openTile); // for start
-        //     var adder = new GoalAndStartAdder();
-        //     adder.ImproveMap(mapMock.Object, 10);
-        //     mapMock.VerifySet(foo => foo.Goal = It.IsAny<Vector2Int>());
-        //     mapMock.VerifySet(foo => foo.Start = It.IsAny<Vector2Int>());
-        // }
+        }
+
+        [Test]
+        public void GoalAndEndShouldBeSet()
+        {
+            SetUpSuccessfulSequence();
+            var adder = new GoalAndStartAdder();
+            adder.ImproveMap(mapMock.Object, 10);
+            mapMock.VerifySet(foo => foo.Goal = It.IsAny<Vector2Int>());
+            mapMock.VerifySet(foo => foo.Start = It.IsAny<Vector2Int>());
+        }
 
 
+        [Test]
+        public void GoalAndEndShouldHaveCorrectTiles()
+        {
+            SetUpSuccessfulSequence();
+            var adder = new GoalAndStartAdder();
+            adder.ImproveMap(mapMock.Object, 10);
+            mapMock.Verify(foo => foo.SetTile(It.IsAny<Vector2Int>(), It.IsAny<GoalTile>()));
+            mapMock.Verify(foo => foo.SetTile(It.IsAny<Vector2Int>(), It.IsAny<StartTile>()));
+        }
 
-        // [Test]
-        // public void GoalAndEndShouldHaveCorrectTiles()
-        // {
-        //     mapMock.Setup(foo => foo.GetTile(It.IsAny<Vector2Int>())).Returns(mockTile);
-        //     var adder = new GoalAndStartAdder();
-        //     adder.ImproveMap(mapMock.Object, 10);
-        //     mapMock.Verify(foo => foo.SetTile(It.IsAny<Vector2Int>(), It.IsAny<GoalTile>()));
-        //     mapMock.Verify(foo => foo.SetTile(It.IsAny<Vector2Int>(), It.IsAny<StartTile>()));
-        // }
+        [Test]
+        public void GoalAndEndShouldNotBeInSameLocation()
+        {
+            SetUpSuccessfulSequence();
+            var startLocation = new Vector2Int(int.MaxValue, int.MaxValue);
+            var goalLocation = new Vector2Int(int.MaxValue, int.MaxValue);
+            mapMock.SetupSet(h => h.Start = It.IsAny<Vector2Int>()).Callback<Vector2Int>(r => startLocation = r);
+            mapMock.SetupSet(h => h.Goal = It.IsAny<Vector2Int>()).Callback<Vector2Int>(r => goalLocation = r);
+            var adder = new GoalAndStartAdder();
+            adder.ImproveMap(mapMock.Object, 10);
 
-        // [Test]
-        // public void GoalAndEndShouldNotBeInSameLocation()
-        // {
-        //     mapMock.Setup(foo => foo.GetTile(It.IsAny<Vector2Int>())).Returns(mockTile);
-        //     var startLocation = new Vector2Int(int.MaxValue, int.MaxValue);
-        //     var goalLocation = new Vector2Int(int.MaxValue, int.MaxValue);
-        //     mapMock.SetupSet(h => h.Start = It.IsAny<Vector2Int>()).Callback<Vector2Int>(r => startLocation = r);
-        //     mapMock.SetupSet(h => h.Goal = It.IsAny<Vector2Int>()).Callback<Vector2Int>(r => goalLocation = r);
-        //     var adder = new GoalAndStartAdder();
-        //     adder.ImproveMap(mapMock.Object, 10);
-            
-        //     Assert.AreNotEqual(startLocation, goalLocation);
+            Assert.AreNotEqual(startLocation, goalLocation);
 
-        // }
+        }
 
     }
 }
