7840e5f0419c417adcf7dce4df539100a052bffb
Martin Favre <martin.favre22@gmail.com>
2021-05-14 12:15:08 +0200

    Add multiplierUpgrade

---
+++ Assets/Scripts/Upgrades/MultiplierUpgrade.cs
@@ -0,0 +1,19 @@
+public class MultiplierUpgrade : Upgrade
+{
+    double MultipliedValue { get => valueMultiplier * Level; }
+    private readonly double valueMultiplier;
+
+    public MultiplierUpgrade(
+        double valueMultiplier,
+        int initialLevel,
+        float baseCost,
+        float costMultiplier,
+        string storageKey,
+        IPersistentDataStorage storage,
+        IPlayerWallet wallet,
+        IUpgradeManager upgradeManager)
+        : base(initialLevel, baseCost, costMultiplier, storageKey, storage, wallet, upgradeManager)
+    {
+        this.valueMultiplier = valueMultiplier;
+    }
+}
