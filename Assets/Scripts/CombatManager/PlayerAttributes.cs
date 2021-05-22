5333ea2e07257393bf0974e2d8522636cbb97340
Martin Favre <martin.favre22@gmail.com>
2021-05-22 10:10:29 +0200

    Store currentHp to DB

--- Assets/Scripts/CombatManager/PlayerAttributes.cs
+++ Assets/Scripts/CombatManager/PlayerAttributes.cs
@@ -6,6 +6,7 @@ public class PlayerAttributes : ICombatAttributes
 {
     private readonly IPersistentDataStorage storage;
     private readonly IEventRecipient<ICharacterUpdateEvent> recipient;
+    private readonly int playerIdentifier;
 
     public double MaxHp { get => maxHp; }
 
@@ -24,11 +25,26 @@ public class PlayerAttributes : ICombatAttributes
     List<MultiplierUpgrade> healthUpgrades;
     List<KeyObserver<string, Upgrade>> attackUpgradeObservers;
     List<KeyObserver<string, Upgrade>> healthUpgradeObservers;
+    IObserver<IPersistentStorageUpdateEvent> storageObserver;
     public PlayerAttributes(IPersistentDataStorage storage, IEventRecipient<ICharacterUpdateEvent> recipient, IUpgradeManager upgradeManager, int playerIdentifier)
     {
         this.storage = storage;
         this.recipient = recipient;
+        this.playerIdentifier = playerIdentifier;
+        InitializeAttackinessUpgrades(upgradeManager);
+        SetAttackiness();
+        InitializeHealthinessUpgrades(upgradeManager);
+        currentHp = storage.GetFloat(GetCurrentHpKey(playerIdentifier), (float)maxHp);
+        storageObserver = new KeyObserver<string, IPersistentStorageUpdateEvent>(storage, GetCurrentHpKey(playerIdentifier), e =>
+        {
+            if (e is DataClearedUpdateEvent clr) currentHp = maxHp;
+            recipient.RecieveEvent(new AttributeChangedEvent(this));
+        });
+        SetMaxHp();
+    }
 
+    private void InitializeAttackinessUpgrades(IUpgradeManager upgradeManager)
+    {
         attackUpgrades = new List<MultiplierUpgrade>()
         {
             new MultiplierUpgrade(5, 1, 50, 1.07f, GetAttackinessUpgradeKey(0, playerIdentifier), storage, PlayerWallet.Instance, upgradeManager),
@@ -38,7 +54,10 @@ public class PlayerAttributes : ICombatAttributes
 
         attackUpgradeObservers = new List<KeyObserver<string, Upgrade>>(attackUpgrades.Count);
         attackUpgrades.ForEach(upgrade => attackUpgradeObservers.Add(new KeyObserver<string, Upgrade>(UpgradeManager.Instance, upgrade.StorageKey, e => SetAttackiness())));
-        SetAttackiness();
+    }
+
+    private void InitializeHealthinessUpgrades(IUpgradeManager upgradeManager)
+    {
         healthUpgrades = new List<MultiplierUpgrade>() {
             new MultiplierUpgrade(50, 1, 50, 1.07f, GetHealthinessUpgradeKey(0, playerIdentifier), storage, PlayerWallet.Instance, upgradeManager),
             new MultiplierUpgrade(500, 0, 1000, 1.09f, GetHealthinessUpgradeKey(1, playerIdentifier), storage, PlayerWallet.Instance, upgradeManager),
@@ -46,8 +65,12 @@ public class PlayerAttributes : ICombatAttributes
         };
         healthUpgradeObservers = new List<KeyObserver<string, Upgrade>>(attackUpgrades.Count);
         healthUpgrades.ForEach(upgrade => attackUpgradeObservers.Add(new KeyObserver<string, Upgrade>(UpgradeManager.Instance, upgrade.StorageKey, e => SetMaxHp())));
-        currentHp = maxHp; // todo, store currenthp in DB
-        SetMaxHp();
+
+    }
+
+    public static string GetCurrentHpKey(int player)
+    {
+        return "currenthpplayer" + player;
     }
 
     public static string GetAttackinessUpgradeKey(int upgradeIndex, int player)
@@ -92,16 +115,26 @@ public class PlayerAttributes : ICombatAttributes
 
     public void Damage(double damage)
     {
+        var oldCurrentHp = currentHp;
         currentHp -= damage;
         if (currentHp <= 0) currentHp = 0;
-        recipient.RecieveEvent(new AttributeChangedEvent(this));
+        if (currentHp != oldCurrentHp)
+        {
+            storage.SetFloat(GetCurrentHpKey(playerIdentifier), (float)currentHp);
+            recipient.RecieveEvent(new AttributeChangedEvent(this));
+        }
     }
 
     public void Heal(double healing)
     {
+        var oldCurrentHp = currentHp;
         currentHp += healing;
         if (currentHp > maxHp) currentHp = maxHp;
-        recipient.RecieveEvent(new AttributeChangedEvent(this));
+        if (currentHp != oldCurrentHp)
+        {
+            storage.SetFloat(GetCurrentHpKey(playerIdentifier), (float)currentHp);
+            recipient.RecieveEvent(new AttributeChangedEvent(this));
+        }
     }
 
 
