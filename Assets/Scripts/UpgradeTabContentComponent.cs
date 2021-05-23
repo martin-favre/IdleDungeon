21c96e20f2decc566efd188f65d827522bbee89c
Martin Favre <martin.favre22@gmail.com>
2021-05-23 18:25:26 +0200

    You can now see name in upgradepanel

--- Assets/Scripts/UpgradeTabContentComponent.cs
+++ Assets/Scripts/UpgradeTabContentComponent.cs
@@ -18,12 +18,16 @@ public class UpgradeTabContentComponent : MonoBehaviour
         public IncreaseUpgradeButton button;
     }
     [SerializeField]
+    TMPro.TMP_Text nameText;
+    [SerializeField]
     UpgradeType upgradeTypes;
     [SerializeField]
     UpgradeButton[] buttons;
 
     public void SetPlayerIndex(int index)
     {
+        var character = SingletonProvider.MainPlayerRoster.GetAllPlayersChars()[index];
+        if(nameText) nameText.text = character.Name;
         foreach (var button in buttons)
         {
             button.button.SetButtonType(index, button.level, upgradeTypes);
--- Assets/Scripts/Upgrades/UpgradePanelComponent.cs
+++ Assets/Scripts/Upgrades/UpgradePanelComponent.cs
@@ -11,6 +11,7 @@ public class UpgradePanelComponent : MonoBehaviour
 
     void Awake() {
         instance = this;
+        gameObject.SetActive(false);
     }
 
     public void OpenPanel(int index)
