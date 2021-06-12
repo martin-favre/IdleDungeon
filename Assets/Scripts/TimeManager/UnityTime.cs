b18c513aec1ec9d2dbea6614baa177289d56fb8a
Martin Favre <martin.favre22@gmail.com>
2021-06-12 15:36:58 +0200

    Proper time control was only needed for an idle game Move to normal unitytime instead now

--- Assets/Scripts/TimeManager/UnityTime.cs
+++ Assets/Scripts/TimeManager/UnityTime.cs
@@ -1,30 +1,17 @@
 using UnityEngine;
 public class UnityTime : ITimeProvider
 {
-    public float Time => timeOffset + UnityEngine.Time.unscaledTime * TimeScaling;
-    public float DeltaTime => timeOffset + UnityEngine.Time.unscaledDeltaTime * TimeScaling;
+    public float Time => UnityEngine.Time.time;
+    public float DeltaTime => UnityEngine.Time.deltaTime;
 
-    private float timeScaling;
     public float TimeScaling
     {
-        get => timeScaling;
-        set
-        {
-            if(value < 0) {
-                Debug.LogWarning("Can't set timescaling to negative value");
-                return;
-            }
-            var oldTime = Time;
-            timeScaling = value;
-            if(Time < oldTime) {
-                timeOffset = oldTime;
-            }
-        }
+        get => UnityEngine.Time.timeScale;
+        set => UnityEngine.Time.timeScale = value;
     }
 
     private static ITimeProvider instance;
 
-    private float timeOffset = 0;
     public static ITimeProvider Instance { get => instance; set => instance = value; }
 
     static UnityTime()
