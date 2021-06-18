cfa87c057b53fca3e53807c61f01de9146072578
Martin Favre <martin.favre22@gmail.com>
2021-06-18 07:04:49 +0200

    Slow down and randomize enemy attack speed Will make it feel a bit more natural It doesn't take the action time into consideration, so I'll have to redo this in the future when we get longer action times

--- Assets/Scripts/CombatManager/LevelGeneratedCharacter.cs
+++ Assets/Scripts/CombatManager/LevelGeneratedCharacter.cs
@@ -13,18 +13,26 @@ class LevelGeneratedCharacter : Character, IHasMaterial, IHasEnemyTemplate
 
     public EnemyTemplate EnemyTemplate => template;
 
+    Dictionary<ICharacterAction, float> nextActionTime = new Dictionary<ICharacterAction, float>(); // hack to make characters sorta feel like they take time thinking about their actions
+    float actionCooldownS = 2;
+
     public LevelGeneratedCharacter(EnemyTemplate template, int currentLevel, float powerFactor) : base(template.Name, null, new LevelGeneratedCombatAttributes(currentLevel, powerFactor), 200)
     {
         healthPoints = new HealthPoints(new WeakReference<ICharacter>(this), 200);
         this.template = template;
         base.characterActions.Add(new AttackRandomAction("Sprites/addon_04", "Attack"));
+        base.characterActions.ForEach(e => nextActionTime[e] = SingletonProvider.MainTimeProvider.Time + actionCooldownS*SingletonProvider.MainRandomProvider.RandomFloat(0.5f, 1.5f));
     }
 
     public override void PerformAction(List<ICharacter> enemies, ICombatReader combat)
     {
         if (characterActions.TrueForAll(e => e.TurnProgress == null))
         {
-            characterActions[0].StartChargingAction(this, combat);
+            if (nextActionTime[characterActions[0]] < SingletonProvider.MainTimeProvider.Time)
+            {
+                characterActions[0].StartChargingAction(this, combat);
+                nextActionTime[characterActions[0]] = SingletonProvider.MainTimeProvider.Time + actionCooldownS*SingletonProvider.MainRandomProvider.RandomFloat(0.5f, 1.5f);
+            }
         }
         IncrementActions(combat);
     }
