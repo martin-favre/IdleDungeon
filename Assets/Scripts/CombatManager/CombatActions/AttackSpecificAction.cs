b14c2de0282c53bfc8afb65c6c44e473c68aba36
Martin Favre <martin.favre22@gmail.com>
2021-06-20 14:03:39 +0200

    Remove redundant target

--- Assets/Scripts/CombatManager/CombatActions/AttackSpecificAction.cs
+++ Assets/Scripts/CombatManager/CombatActions/AttackSpecificAction.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class AttackSpecificAction : BaseCharacterAction
 {
-    private ICharacter target;
 
     public AttackSpecificAction(string icon, string name) : base(icon, name)
     {
     }
 
-    public ICharacter Target { get => target; set => target = value; }
-
     public override void PerformAction(ICharacter user, ICombatReader combat)
     {
-        if (target != null && !target.IsDead())
+        if (Target != null && !Target.IsDead())
         {
-            target.BeAttacked(user.Attributes.Attack);
-            CentralEventPublisher.Instance.Publish(EventType.CombatAction, new CombatActionEvent(combat, target, user, this));
+            Target.BeAttacked(user.Attributes.Attack);
+            CentralEventPublisher.Instance.Publish(EventType.CombatAction, new CombatActionEvent(combat, Target, user, this));
         }
     }
 }
